f7d16a59a050b62824ad0125eaa5cd37c2dc8bf1
Yorday Figueroa <yfsanchez@gmail.com>
2021-04-02 17:53:18 -0300

    clase 359
    creando un stream observable

--- src/main/java/com/yfsanchez/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/yfsanchez/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,25 @@
 package com.yfsanchez.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Nombre01","Nombre02","Nombre03","Nombre04")
+				.doOnNext(nombre -> System.out.println(nombre));
+		
+		nombres.subscribe();
+		
 	}
 
 }
